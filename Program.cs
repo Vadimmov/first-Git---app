063c90cc540721d5c472bdcc87abab2d12331cb7
Вадим <sagalakovvadim15@gmail.com>
2026-04-27 17:15:00 +0700

    Добавлена первая программа

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ class HelloGit
 {
     static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
         Console.WriteLine("Привет, Git-сервис!");
         Console.WriteLine($"Дата: {DateTime.Now:dd.MM.yyyy}");
     }
